c8b8b44630ed51c1333f7d8d7ca52690185559d4
Yangming Huang <yangming@coscene.io>
2023-01-04 15:05:28 +0800

    merge dp and filter

--- dp.py
+++ dp.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 import argparse
+import json
 import os
 import re
 import subprocess
 import sys
 from pathlib import Path
 from dataclasses import dataclass
-from typing import Tuple, List
+from typing import Iterator, Tuple, List
 
 CONF_DIR = os.path.expanduser("~/.config/dp")
 P_CONF = re.compile(r'"([^"]+)"', re.IGNORECASE)
@@ -16,6 +17,9 @@ P_ATTR = re.compile(r'":"', re.IGNORECASE)
 
 @dataclass
 class Config:
+    """
+    Config for a single display
+    """
     id_: str
     res: Tuple[int, int]
     origin: Tuple[int, int] = (0,0)
@@ -43,6 +47,9 @@ class Config:
 
 @dataclass
 class Layout:
+    """
+    Layout is a group of configs
+    """
     configs: List[Config]
 
     @staticmethod
@@ -63,32 +70,68 @@ class Layout:
     def run(self):
         subprocess.run(self.to_command(), shell=True, check=True)
 
-    def switch(self, templates):
-        if self.footprint == templates[0].footprint:
+    def switch(self, template_layouts):
+        if self.footprint == template_layouts[0].footprint:
             print("switch to 1", file=sys.stderr)
-            self.configs = templates[1].configs
+            self.configs = template_layouts[1].configs
         else:
             print("switch to 0", file=sys.stderr)
-            self.configs = templates[0].configs
+            self.configs = template_layouts[0].configs
 
 
-def save_templates(template_path: Path, layout: Layout):
-    template_path.parent.mkdir(parents=True, exist_ok=True)
-    with template_path.open('a') as f:
-        f.write(layout.to_command())
-        f.write("\n")
+class Template:
+    def __init__(self, template_file) -> None:
+        self._template_path = Path(template_file).expanduser()
+        
+    def save(self, layout: Layout):
+        self._template_path.parent.mkdir(parents=True, exist_ok=True)
+        with self._template_path.open('a') as f:
+            f.write(layout.to_command())
+            f.write("\n")
+
+    def load(self):
+        return [Layout.parse(l) for l in self.load_raw()]
+
+    def load_raw(self):
+        if self._template_path.exists():
+            with self._template_path.open() as f:
+                for l in f.readlines():
+                    if l.strip():
+                        yield l
 
 
-def load_templates(template_path: Path):
-    with template_path.open() as f:
-        return [Layout.parse(l)
-                for l in f.readlines()
-                if l.strip()]
+class Alfred:
+    @staticmethod
+    def _to_alfred_list(raws: Iterator[str]):
+        items = [
+            {
+                "title": "Auto Switch",
+                "subtitle": "Auto switch based on the current config",
+                "arg": "!",
+                "autocomplete": "auto"
+            }
+        ] + [
+            {
+                "title": f"Config {i}",
+                "subtitle": raw,
+                "arg": raw,
+                "autocomplete": i
+            }
+            for i, raw in enumerate(raws)
+        ]
+
+        return {"items": items}
+
+    @staticmethod
+    def script_filter(template: Template):
+        raws = template.load_raw()
+        print(json.dumps(Alfred._to_alfred_list(raws)))
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', "--save", action='store_true')
+    parser.add_argument('-a', "--alfred", action='store_true', help="run as alfred script filter")
     parser.add_argument('-p', "--print-only", action='store_true')
     parser.add_argument('template_file', nargs='?', type=str, default=f"{CONF_DIR}/templates.txt")
     return parser.parse_args()
@@ -99,12 +142,14 @@ def main():
     result = subprocess.run(['displayplacer', 'list'], capture_output=True, text=True)
     layout = Layout.parse(result.stdout)
 
-    template_path = Path(args.template_file).expanduser()
-    if args.save:
-        save_templates(template_path, layout)
+    template = Template(args.template_file)
+    if args.alfred:
+        Alfred.script_filter(template)
+    elif args.save:
+        template.save(layout)
     else:
-        templates = load_templates(template_path)
-        layout.switch(templates)
+        template_layouts = template.load()
+        layout.switch(template_layouts)
         print(layout.to_command())
         if not args.print_only:
             layout.run()
